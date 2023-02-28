dde212856a1d79aedb5340c9388d0068e648cf95
Oleg Istomin <Oleg.Istomin@infobip.com>
2023-02-28 03:30:24 +0300

    upd

--- install-cert.sh
+++ install-cert.sh
@@ -1,3 +1,3 @@
 DOMAIN='vooh.ru'
-RUN mkdir -p /etc/haproxy/certs/
+mkdir -p /etc/haproxy/certs/
 cat /etc/letsencrypt/live/$DOMAIN/fullchain.pem /etc/letsencrypt/live/$DOMAIN/privkey.pem > /etc/haproxy/certs/$DOMAIN.pem
